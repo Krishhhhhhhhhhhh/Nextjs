ecf7ecf2d1fb85ea4bdcb166a21622f27ab6bae7
krishna <pathakrishna2704@gmail.com>
2026-02-18 22:04:36 +0530

    Add SignupPage component with initial layout and imports

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react"
+import {useRouter} from "next/navigation"
+import {axios} from "axios";
+
+
+export default function SignupPage(){
+    const [user,setUser]=react
+    return (
+        <div className="flex">
+            <h1 className="text-center text-white text-2xl">Signup</h1>
+        </div>
+    )
+}
